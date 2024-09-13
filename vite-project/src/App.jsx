1db76bbcc5854ad31bac897c0c7eb6413617e168
Erikvilar <erikvilaralves@gmail.com>
2024-09-13 12:57:25 -0300

    Projeto react onde exibe as caracteristicas de um usuario

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+import UserProfile from './UserProfile/UserProfile';
+
+function App() {
+
+
+  return (
+
+<UserProfile/>
+
+  )
+}
+
+export default App
---
+++ vite-project/src/UserProfile/UserProfile.jsx
@@ -0,0 +1,56 @@
+import css from "./UserProfile.module.css";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const UserProfile = () => {
+  const UserProfile = ({ user }) => {
+
+
+
+    const [name, setName] = useState(user.name);
+
+    const { userX } = { ...name };
+
+
+    useEffect(() => {
+      setName(user.name); o
+    }, [user]);
+
+
+    const unusedFunction = () => {
+      console.log("This function is never used");
+    };
+
+
+    useEffect(() => {
+      setName(user.name);
+    }, []);
+
+
+    return (
+      <div>
+        <h1>{name}</h1>
+        {user.hobbies.map((hobby) => (
+          <p>{hobby}</p> 
+        ))}
+        <button
+          onClick={() => {
+          }}
+        >
+          Update Profile
+        </button>
+        <div style={{ color: "red" }}>Red text</div>{" "}
+ 
+      </div>
+    );
+  };
+
+
+  UserProfile.propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      hobbies: PropTypes.arrayOf(PropTypes.string),
+    }).isRequired,
+  };
+};
+export default UserProfile;
