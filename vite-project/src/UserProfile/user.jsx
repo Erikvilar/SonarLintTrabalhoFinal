af5c9663455e85cb8d93144e18b0e7783f168ec3
Erikvilar <erikvilaralves@gmail.com>
2024-09-13 13:38:45 -0300

    ao se resolver esses problemas o sonarLint já não acusa erros mais pois ao se transformar user em um objeto parte da logica já esta resolvida

--- vite-project/src/UserProfile/user.jsx
+++ vite-project/src/UserProfile/user.jsx
@@ -3,7 +3,7 @@ const user =[
     {id:2, name:"joao", hobby:"jogar"},
     {id:3, name:"vitor", hobby:"correr"},
     {id:4, name:"lucas", hobby:"dançar"},
-    {id:5, name:"alex", hobby:"comer"},
+
 
 ]
 export default user
