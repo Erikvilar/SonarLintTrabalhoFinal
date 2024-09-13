ded13593c25c280dd3c761229d4e719f40bb067d
Erikvilar <erikvilaralves@gmail.com>
2024-09-13 13:36:34 -0300

    Passada a key prop id para o component pai pessoa.id fazendo a indentificação unica de cada objeto

--- vite-project/src/UserProfile/UserProfile.jsx
+++ vite-project/src/UserProfile/UserProfile.jsx
@@ -1,24 +1,17 @@
-
 import user from "./user";
-import css from "./UserProfile.module.css"
+import css from "./UserProfile.module.css";
 const UserProfile = () => {
- 
-    
-
-
-    return (
-
-      <div className={css.user}>
-		{user.map((pessoa)=>(
-		<div key={pessoa.id}>
-        <h1>{pessoa.name} gosta de {pessoa.hobby}</h1>
-		<button>+Veja mais sobre {pessoa.name}</button>
-		</div>
-	))}
-
-      </div>
-	  
-    );
-	
-  };
+  return (
+    <div>
+      {user.map((pessoa) => (
+        <div key={pessoa.id}  className={css.user}>
+          <h1>
+            {pessoa.name} gosta de {pessoa.hobby}
+          </h1>
+          <button>+Veja mais sobre {pessoa.name}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default UserProfile;
--- vite-project/src/UserProfile/user.jsx
+++ vite-project/src/UserProfile/user.jsx
@@ -1,4 +1,9 @@
 const user =[
-    {id:1, name:"erik", hobby:"esquiar"}
+    {id:1, name:"erik", hobby:"esquiar"},
+    {id:2, name:"joao", hobby:"jogar"},
+    {id:3, name:"vitor", hobby:"correr"},
+    {id:4, name:"lucas", hobby:"dançar"},
+    {id:5, name:"alex", hobby:"comer"},
+
 ]
 export default user
